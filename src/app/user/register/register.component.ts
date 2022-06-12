de04b3944be69b256d1e81434bf71f3baee214f7
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-12 11:18:14 +0300

    move the controls and validators to new component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,18 +8,26 @@ import { FormGroup, FormControl, Validator, Validators  } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent  {
-    registerForm = new FormGroup({
       // we can put some validators in the template but
       // its better to let angular handle them, and we should choose
       // to put them in the class or the template
-      name  : new FormControl('',[
+      name  = new FormControl('',[
         Validators.required,
         Validators.minLength(3),
-      ]),
-      email : new FormControl(''),
-      age : new FormControl(),
-      password : new FormControl(),
-      confirm_password : new FormControl(),
-      phone : new FormControl(),
+      ]);
+      email = new FormControl('');
+      age = new FormControl();
+      password = new FormControl();
+      confirm_password = new FormControl();
+      phone = new FormControl();
+
+
+    registerForm = new FormGroup({
+      name:  this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      conftirm_password: this.confirm_password,
+      phone: this.phone
     });
 }
