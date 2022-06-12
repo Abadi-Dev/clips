eb152f4684042859fdbf6831ccaff53756a4ff12
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-12 12:36:54 +0300

    add other validators to the component

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { SharedModule } from '../shared.module';
+
 
 
 @Component({
@@ -10,7 +10,8 @@ import { SharedModule } from '../shared.module';
 })
 export class InputComponent implements OnInit {
   @Input() control : FormControl = new FormControl();
-
+  @Input() type = 'text';
+  @Input() placeholder = "";
   constructor() { }
 
   ngOnInit(): void {
