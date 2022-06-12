7a3c603a204785a8450b0c1faa18863373a1605e
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-12 10:07:25 +0300

    add validators to the field

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl  } from '@angular/forms';
+import { FormGroup, FormControl, Validator, Validators  } from '@angular/forms';
 
 
 @Component({
@@ -9,7 +9,13 @@ import { FormGroup, FormControl  } from '@angular/forms';
 })
 export class RegisterComponent  {
     registerForm = new FormGroup({
-      name  : new FormControl(''),
+      // we can put some validators in the template but
+      // its better to let angular handle them, and we should choose
+      // to put them in the class or the template
+      name  : new FormControl('',[
+        Validators.required,
+        Validators.minLength(3),
+      ]),
       email : new FormControl(''),
       age : new FormControl(),
       password : new FormControl(),
