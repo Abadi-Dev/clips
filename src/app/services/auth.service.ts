ae5b652f0bbf040d197afabf141d413c888a4c45
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-19 14:24:56 +0300

    check if user is authenticated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
 import IUser from '../models/user.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private userCollection : AngularFirestoreCollection<IUser>;
+  public isAuthenticated$ : Observable<boolean>
+
   constructor(private auth: AngularFireAuth,  private db: AngularFirestore) {
      this.userCollection = this.db.collection('users');
+     auth.user.subscribe(console.log);
+     this.isAuthenticated$ = auth.user.pipe(
+       map((value) => {return !!value})
+     )
    }
 
 
