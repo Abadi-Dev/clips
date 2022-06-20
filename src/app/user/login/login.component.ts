847b6c4959a326aae6763be0e52f95c8d8e7df00
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-20 11:13:16 +0300

    work on auth

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
+  constructor(auth: AngularFireAuth) {}
   credentials = {
-    email : "",
-    password: ""
-  }
-  register(){
+    email: '',
+    password: '',
+  };
+  async login() {
     console.log('login is done');
-
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
