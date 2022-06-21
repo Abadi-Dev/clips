4e0348865ff6802bf52dfe698cf216ae96691b35
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-21 10:56:56 +0300

    finished the login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,12 +7,38 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(auth: AngularFireAuth) {}
+  constructor(private auth: AngularFireAuth) {}
   credentials = {
     email: '',
     password: '',
   };
+
+  alertColor = '';
+  showAlert = false;
+  alertMsg = 'please wait while we log you in';
+  showSumbitButton = false;
+
   async login() {
+    this.alertColor = 'blue';
+    this.showAlert = true;
+    this.alertMsg = 'please wait while we log you in';
+    this.showSumbitButton = true;
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (error: any) {
+      this.alertMsg = error.message;
+      this.alertColor = 'red';
+      this.showAlert = true;
+      this.showSumbitButton = false;
+      return;
+    }
+    this.alertColor = 'green';
+    this.showAlert = true;
+    this.showSumbitButton = true;
+    this.alertMsg = 'logged in';
     console.log('login is done');
   }
 
