87f967499622fd017dab125a362ab6ed7748cbf9
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-29 09:59:59 +0300

    add static method (unfineshed)

--- src/app/user/Validators/register-validator.ts
+++ src/app/user/Validators/register-validator.ts
@@ -1,2 +1,7 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 export class RegisterValidator {
+  static match(group: AbstractControl): ValidationErrors | null {
+    // TODO: compare the value from the "password" field to the value in the "confirm password field"
+    return null;
+  }
 }
