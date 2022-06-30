2b7379c8f99cf3d487120b3f339d7fc0a8efd8fb
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-30 13:49:08 +0300

    add async validator if the email is taken

---
+++ src/app/user/Validators/email-taken.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from '@angular/forms';
+import { promises } from 'dns';
+import { Http2ServerRequest } from 'http2';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class EmailTaken implements AsyncValidator {
+  constructor(private auth: AngularFireAuth) {}
+
+  validate(control: AbstractControl): Promise<ValidationErrors | null> {
+    return this.auth
+      .fetchSignInMethodsForEmail(control.value)
+      .then((response) => (response.length ? { emailTaken: true } : null));
+  }
+}
