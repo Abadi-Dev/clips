8db4ac19d44bdacf65dea7d818b8fd8b0f43a327
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-30 09:53:37 +0300

    finish the function

--- src/app/user/Validators/register-validator.ts
+++ src/app/user/Validators/register-validator.ts
@@ -1,7 +1,12 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 export class RegisterValidator {
   static match(group: AbstractControl): ValidationErrors | null {
-    // TODO: compare the value from the "password" field to the value in the "confirm password field"
-    return null;
+    const control = group.get('password');
+    const matchingControl = group.get('confirm_password');
+    if (!matchingControl || !control) {
+      return { controlNotFound: false };
+    }
+    const error = matchingControl === control ? null : { noMatch: true };
+    return error;
   }
 }
