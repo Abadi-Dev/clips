e5db397fb858d8ccaa96c720149835883ff7e6d1
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-07-10 20:19:17 +0300

    add route to manage component by importing the video module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,34 @@ import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SharedModule } from './shared/shared.module';
-import {AngularFireModule} from "@angular/fire/compat"
+import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
-import {AngularFireAuthModule} from '@angular/fire/compat/auth/'
-import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth/';
+import {
+  AngularFirestore,
+  AngularFirestoreModule,
+} from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
+import { AboutComponent } from './about/about.component';
+import { VideoModule } from './video/video.module';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule, SharedModule, AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule, AngularFirestoreModule],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    AboutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    VideoModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
