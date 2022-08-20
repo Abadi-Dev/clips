e7daae1402a32da0ce4e03f510c5b0b467f3b715
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-08-21 00:43:42 +0300

    sorting videos

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
@@ -7,9 +7,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./manage.component.css'],
 })
 export class ManageComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {
-    this.route.data.subscribe(console.log);
-  }
+  videoOrder = '1'
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params: Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1'
+    })
+  }
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement)
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value
+      }
+    })
+  }
 }
