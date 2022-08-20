3fd4360ac4ef0b80e3e368afe0b6e361da29c919
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-08-20 21:20:59 +0300

    add template and update component

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clip',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clip.component.css']
 })
 export class ClipComponent implements OnInit {
-
-  constructor() { }
+  id = '';
+  constructor(public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id
+    })
   }
 
 }
