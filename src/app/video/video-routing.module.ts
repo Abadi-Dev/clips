be57cc38d346ae3c25c494efc5aab6ceaccd1642
Abadi-Dev <65283737+Abadi-Dev@users.noreply.github.com>
2022-08-21 02:02:08 +0300

    route guards

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
 
 const routes: Routes = [
   {
@@ -9,15 +12,19 @@ const routes: Routes = [
     component: ManageComponent,
     data: {
       authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
     },
+    canActivate: [AngularFireAuthGuard]
   },
   {
     path: 'upload',
     component: UploadComponent,
     data: {
-      authOnly: true
-    }
-  }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AngularFireAuthGuard]
+  },
 ];
 
 @NgModule({
