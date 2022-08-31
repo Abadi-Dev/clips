e0d99d1acacac8f7712601f573ad44399d2c970b
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-08-31 14:23:55 +0300

    reactive form

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, RequiredValidator, Validators } from '@angular/forms';
+import { v4 as uuid } from "uuid";
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -8,6 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class UploadComponent implements OnInit {
 
   isDragover = false;
+  file: File | null = null;
+  nextStep = false;
+
+
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+    ],
+    nonNullable: true
+  })
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+
   constructor() { }
 
   ngOnInit(): void {
@@ -15,6 +33,19 @@ export class UploadComponent implements OnInit {
 
   storeFile($event: Event) {
     this.isDragover = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if (!this.file || this.file.type !== 'image/png') {
+      return
+    }
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
+    this.nextStep = true;
   }
 
+  uploadFile() {
+    // const cl
+    // const clipPath = `clips/${this.file?.name}`;
+  }
 }
