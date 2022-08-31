56b2ccec55eb41ec8924950589e45aefad9bc104
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-08-31 12:15:24 +0300

    upload component and routing

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
 
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
+    data: {
+      authOnly: true,
+    },
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
     data: {
       authOnly: true,
     },
@@ -16,4 +24,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class VideoRoutingModule {}
+export class VideoRoutingModule { }
